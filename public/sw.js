5555f13fcd72ba69cb67f6812622ae9713754ee2
suraj1padhi787 <suraj1padhi@gmail.com>
2025-04-11 14:51:01 +0530

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,29 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('chat-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/chat',
-          '/css/style.css',
-          '/js/main.js',
-          '/manifest.json'
-        ]);
-      })
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('chat-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/chat',
+        '/css/style.css',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// ✅ Handle push notification
+self.addEventListener('push', e => {
+  const data = e.data.json();
+  self.registration.showNotification("Love se message aaya 💌", {
+    body: data.body,
+    icon: '/icons/app-icon.png'
   });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+});
